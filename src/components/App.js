4e187ca413a07b904ab7c8a09d5bdf6d654209bb
Kun Zhang <kunzhang1988@gmail.com>
2020-03-12 17:21:59 +1000

    Configure Connect with MapStateToProps

--- src/components/App.js
+++ src/components/App.js
@@ -2,9 +2,14 @@ import React from 'react';
 // Don't need to add the file name if it is index file ../actions/index
 // {} for named export
 import { selectSong } from '../actions';
+import SongList from './SongList';
 
 const App = () => {
-    return <div>App</div>
+    return (
+        <div>
+            <SongList />
+        </div>
+    );
 };
 
 export default App;
